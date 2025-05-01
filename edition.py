254de3637c97575639c82b3d5ca1a9dc489b60a1
gcapoccia6190 <146028121+gcapoccia6190@users.noreply.github.com>
2025-04-30 23:10:19 -0400

    Delete edition.py

--- edition.py
+++
@@ -1,8 +0,0 @@
-# --- edition.py ---
-from baseObject import BaseObject
-
-class Edition(BaseObject):
-    def __init__(self):
-        table = 'Edition'
-        fields = ['EditionID', 'BookID', 'EditionNumber', 'PublishYear', 'Format']
-        super().__init__(table, fields)
